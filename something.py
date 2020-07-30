64ab03a5cae509fd3097a3f9de90bf23a44cdb76
devil <vthang4799>
2020-07-30 15:51:34 +0800

    Add word count function

---
+++ something.py
@@ -0,0 +1,9 @@
+""" couting words"""
+def main():
+    print(count_words("A fox jumps over a ledge and falls flat"))
+
+def count_words(phrase):
+    words = phrase.split()
+    return len(word)
+if __name__== '__name--'
+    main()
